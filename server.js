2e025ebf052ccb01f9508f7e4c046d3e1981a2d1
Abdisalan Abdullahi Mohamed <mohamed.abdisalan@graduate.utm.my>
2026-01-03 13:34:48 +0300

    MongoDB

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoUsersRoute from "./routes/userRoute.js";
 import { authMiddleware } from "./middlewares/auth.middleware.js";
-dotenv.config();
-const app = express();
-
 import userRoutes from "./routes/user.routes.js";
 
-const PORT = process.env.APP_PORT;
+import { connectDB } from "./db/mongo.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.APP_PORT || 3000;
 
 app.use(express.json());
 
-app.use("/api", authMiddleware, userRoutes);
+app.use("/ormapi", authMiddleware, userRoutes);
+app.use("/odmapi", mongoUsersRoute);
+
+await connectDB();
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
